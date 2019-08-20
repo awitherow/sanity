097e947d4bafc44a4886907679f133cba0ae5725
awitherow <au.witherow@gmail.com>
2019-08-19 16:08:33 -1000

    add subtitle

--- schemas/seo.js
+++ schemas/seo.js
@@ -6,6 +6,12 @@ export default [
     type: "string",
   },
   {
+    name: "subtitle",
+    title: "Subtitle",
+    description: "format this in a way that is SEO friendly and optimized."
+    type: "string",
+  },
+  {
     name: "description",
     title: "Description",
     description: "format this in a way that is SEO friendly and optimized."
