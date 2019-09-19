e21685b38990f2b4e0fff581e0b307a5d613aaf0
awitherow <au.witherow@gmail.com>
2019-09-19 11:21:20 -1000

    remove unnecessary subtitle

--- schemas/seo.js
+++ schemas/seo.js
@@ -6,12 +6,6 @@ export default [
     type: "string",
   },
   {
-    name: "subtitle",
-    title: "Subtitle",
-    description: "format this in a way that is SEO friendly and optimized.",
-    type: "string",
-  },
-  {
     name: "description",
     title: "Description",
     description: "format this in a way that is SEO friendly and optimized.",
